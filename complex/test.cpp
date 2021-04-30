6c6622a489669d3f1c03a4309050f02f34ae6bbe
Garik57 <79136701+Garik57@users.noreply.github.com>
2021-04-30 09:33:11 +0300

    Update test.cpp

--- complex/test.cpp
+++ complex/test.cpp
@@ -1,59 +1,96 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
 
 using namespace std;
 
-class Complex {
+template<typename T1, typename T2>
+class Pair {
 public:
-    Complex() {
+    T1 first;
+    T2 second;
+    Pair() {
     }
-
-    Complex(const double real, const double imaginary) {
+    Pair(const T1 _first, const T2 _second) {
+        first = _first;
+        second = _second;
     }
-
-    Complex(const Complex& copied) {
+    Pair(const Pair& copied) {
+        first = copied.first;
+        second = copied.second;
     }
-
-    Complex& operator=(const Complex& copied) {
+    Pair& operator=(const Pair& copied) {
+        first = copied.first;
+        second = copied.second;
+        return (*this);
     }
-
-    ~Complex() {
+    ~Pair() {
     }
-
-    bool operator!=(const Complex& other) const {
+    bool operator!=(const Pair& other) const {
+        if (first != other.first || second != other.second) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    bool operator==(const Complex& other) const {
+    bool operator==(const Pair& other) const {
+        if (first == other.first && second == other.second) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    Complex operator-() const {
+    bool operator<(const Pair& other) const {
+        if (first < other.first) {
+            return true;
+        } else if (first > other.first) {
+            return false;
+        } else if(second < other.second) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    Complex operator+(const Complex& other) const {
-    }
-
-    Complex operator-(const Complex& other) const {
-    }
-
-    Complex operator*(const Complex& other) const {
-    }
-
-    Complex operator/(const Complex& other) const {
-    }
-
-    double module() const {
-    }
-
-    double argument() const {
-    }
-
-    Complex Conjugate() const {
-    }
-
-private:
-    double _real, _imaginary;
 };
 
-int main() {
+template<typename T>
+void mySort(vector<T>& a) {
+    int n = a.size();
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(a[j + 1] < a[j]){
+                T c = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = c;
+            }
+        }
+    }
+}
 
-    return 0;
+int main() {
+    srand(time(NULL));
+    int n = 10 + rand() % 10;
+    vector<pair<int, double> > a(n);
+    vector<Pair<int, double> > b(n);
+    for (int i = 0; i < n; ++i) {
+        a[i].first = rand() % 100;
+        a[i].second = 0.5 + rand() % 100;
+        b[i].first = a[i].first;
+        b[i].second = a[i].second;
+    }
+    sort(a.begin(), a.end());
+    mySort(b);
+    for (int i = 0; i < n; ++i) {
+        cout << a[i].first << " " << a[i].second << " " << b[i].first << " " << b[i].second << endl;
+    }
+    for (int i = 0; i < n; ++i) {
+        if (a[i].first != b[i].first) {
+            cout << "a[" << i << "].first != b[" << i << "].first: " << a[i].first << " != " << b[i].first << endl;
+        }
+        if (a[i].second != b[i].second) {
+            cout << "a[" << i << "].second != b[" << i << "].second: " << a[i].second << " != " << b[i].second << endl;
+        }
+    }
 }
